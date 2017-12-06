ba5b955c95048ab0d8c3a2050251c8c599d47c4e
neikvon <neikvon@hotmail.com>
2017-12-06 11:38:06 +0800

    test: datetime format

--- test/utils/date-format.js
+++ test/utils/date-format.js
@@ -16,7 +16,7 @@ test('/', t => {
 test('年月日 时分秒', t => {
   const dt = 'Fri Dec 01 2017 13:44:42 GMT+0800'
   const formated = dateFormat(dt, 'YYYY年MM月DD日 hh时mm分ss秒')
-  t.is(formated, '2017年12月01日 05')
+  t.is(formated, '2017年12月01日 05时44分42秒')
 })
 
 test('string', t => {
