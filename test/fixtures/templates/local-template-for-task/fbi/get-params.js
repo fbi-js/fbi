57d545553d45b49d71ff7aa0b33303f8e9d246d5
neikvon <neikvon@gmail.com>
2020-02-11 19:03:28 +0800

    init: cleanup for new version

--- test/fixtures/templates/local-template-for-task/fbi/get-params.js
+++
@@ -1,6 +0,0 @@
-/* global ctx */
-
-const params1 = ctx.task.getParams()
-const params2 = ctx.task.getParams('get-params')
-const params3 = ctx.task.getParams('get-params', 't')
-console.log(params1, params2, params3)
